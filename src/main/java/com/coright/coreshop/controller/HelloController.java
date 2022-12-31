39f4474acf9974abc1ebcb2ce48ecfbf47780dc5
cororing <corock.dev@gmail.com>
2022-12-31 19:12:46 +0900

    feat: Print "Hello, World!"

---
+++ src/main/java/com/coright/coreshop/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.coright.coreshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// NOTE: @RestController == @RequestBody + @Controller
+@RestController
+public class HelloController {
+
+    // @GetMapping
+    // @GetMapping(value = "/")
+    // @GetMapping(path = "/")
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, world!";
+    }
+
+}
