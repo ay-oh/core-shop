75db0c757497722c45b3e57c065903efd3cc9e9b
cororing <corock.dev@gmail.com>
2023-02-04 15:49:01 +0900

    feat: 쿼리 메서드 추가 및 테스트 코드 작성

--- src/main/java/com/ayoh/coreshop/repository/ProductRepository.java
+++ src/main/java/com/ayoh/coreshop/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.ayoh.coreshop.repository;
 
 import com.ayoh.coreshop.entity.product.Product;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByName(String name);
+
 }
--- src/test/java/com/ayoh/coreshop/repository/ProductRepositoryTest.java
+++ src/test/java/com/ayoh/coreshop/repository/ProductRepositoryTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.ayoh.coreshop.entity.product.Product;
 import com.ayoh.coreshop.entity.product.ProductSellStatus;
 import java.time.LocalDateTime;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +21,8 @@ class ProductRepositoryTest {
     @Autowired
     ProductRepository productRepository;
 
-    @Test
     @DisplayName("상품 저장 테스트")
+    @Test
     void testSave() {
         // given
         Product product = new Product();
@@ -37,6 +39,34 @@ class ProductRepositoryTest {
 
         // then
         assertThat(savedProduct.getName()).isEqualTo("테스트 상품");
+    }
+
+    @DisplayName("상품명 조회 테스트")
+    @Test
+    void findByName() {
+        // given
+        this.saveProducts();
+
+        // when
+        List<Product> products = productRepository.findByName("테스트 상품 2");
+
+        // then
+        Assertions.assertThat(products).isNotNull().hasSize(1);
+    }
+
+    private void saveProducts() {
+        for (int i = 0; i < 3; i++) {
+            Product product = new Product();
+            product.setName("테스트 상품 " + i);
+            product.setPrice(10_000);
+            product.setStockNumber(100);
+            product.setDetails("테스트 상품 상세 설명");
+            product.setSellStatus(ProductSellStatus.FOR_SALE);
+            product.setCreatedDate(LocalDateTime.now());
+            product.setUpdatedDate(LocalDateTime.now());
+
+            productRepository.save(product);
+        }
     }
 
 }
